92e770727ccec92b3320a7a5dfffba5092083b2c
GugaSantos <guga.santos.guga@gmail.com>
2022-02-09 00:45:03 -0300

    Requisiçoes feitas e inicio da criaçao das sessions

--- SpotiTrybe/script.js
+++ SpotiTrybe/script.js
@@ -20,7 +20,46 @@ const getToken = async () => {
 };
 // -----------------------------------------------------------------------
 
+//------------------Faz a Requisição das categorias-----------------------
+const getGenres = async (token) => {
+  const requestInfo = {
+    method: 'GET',
+    headers: {
+      Authorization:`Bearer ${token}`
+    }
+  };
+
+  const url = 'https://api.spotify.com/v1/browse/categories?locale=pt-br'
+  const response = await fetch(url, requestInfo);
+  const data = await response.json();
+  return data.categories.items;
+  
+};
+// -----------------------------------------------------------------------
+
+const renderGenres = (genres) => {
+  const genresCards = document.querySelector('.genre-cards');
+
+  genresCards.forEach((genre) => {
+    const section = document.createElement('section');
+    section.className = 'genre';
+    section.id = genre.id;
+
+    const paragraph = document.createElement('p');
+    paragraph.className = 'genre-title';
+    paragraph.innerHTML = genre.name;
+
+    const img = document.createElement('img');
+    img.className = 'genre-image';
+    img.src = genre.icons[0].url;
+  });
+
+  
+
+};
+
 window.onload = async () => {
   token = await getToken();
+  const genres = await getGenres(token);
 };
 
